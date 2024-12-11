3541a3e89138a8de6aa216412f816bcf0c2663c6
Jason Moore <jmoore@austwa.com>
2024-12-11 15:23:28 +0800

    Update legacyuat.dbca.env.js

--- dist/release/static/js/legacyuat.dbca.env.js
+++ dist/release/static/js/legacyuat.dbca.env.js
@@ -3,12 +3,12 @@ var env = {
 
     appType: (window.location.protocol == "file:")?"cordova":"webapp",
 
-    oldcswService:"https://csw-uat.dbca.wa.gov.au/catalogue/api2/application/records",
-    cswService : "https://csw-uat.dbca.wa.gov.au/catalogue/api/records/",
+    oldcswService:"https://csw.dbca.wa.gov.au/catalogue/api2/application/records",
+    cswService : "https://csw.dbca.wa.gov.au/catalogue/api/records/",
     catalogueAdminService:"https://csw-uat.dbca.wa.gov.au",
 
-    kmiService:"https://kmi-uat.dbca.wa.gov.au/geoserver",
-    legendSrc:"https://kmi-uat.dbca.wa.gov.au/geoserver/gwc/service/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&legend_options=fontName:Times%20New%20Roman;fontAntiAliasing:true;fontSize:14;bgColor:0xFFFFEE;dpi:120;labelMargin:10&LAYER=",
+    kmiService:"https://kmi.dbca.wa.gov.au/geoserver",
+    legendSrc:"https://kmi.dbca.wa.gov.au/geoserver/gwc/service/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&legend_options=fontName:Times%20New%20Roman;fontAntiAliasing:true;fontSize:14;bgColor:0xFFFFEE;dpi:120;labelMargin:10&LAYER=",
 
 	  hotspotService:"https://hotspots-uat.dbca.wa.gov.au/geoserver/hotspots/ows",
 
